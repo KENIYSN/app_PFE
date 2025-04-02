ab0e3757ef703edd1a4511ce8021a628fd542345
keniYsn <yassineassardaoui906@gmail.com>
2025-04-03 00:21:15 +0200

    Ajout du dossier .expo

--- app/(tabs)/signUp.tsx
+++ app/(tabs)/signUp.tsx
@@ -9,7 +9,7 @@ import {
   KeyboardAvoidingView,
   TouchableWithoutFeedback,
   Keyboard,
-  ScrollView
+  
 } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import { format } from 'date-fns';
